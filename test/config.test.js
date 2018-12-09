28f7ac61e2980430c65ad7c95170ea1250a955d6
fbernard <fbernard@appstud.com>
2018-12-09 21:13:57 +0100

    add unit test and change version

---
+++ test/config.test.js
@@ -0,0 +1,49 @@
+const rewire = require('rewire')
+const isApplicable = rewire('../index.js').__get__('isApplicable')
+
+describe('test configuration applicability', () => {
+  it('without prefix and request match', () => {
+    var mockRequest = {
+      headers: { 'content-type': 'application/grpc-web-text' },
+      method: 'POST',
+      path: '/test/path'
+    }
+    expect(isApplicable(mockRequest)).toBe(true)
+  })
+
+  it('without prefix and request content-type does not match', () => {
+    var mockRequest = {
+      headers: { 'content-type': 'application/json' },
+      method: 'POST',
+      path: '/test/path'
+    }
+    expect(isApplicable(mockRequest)).toBe(false)
+  })
+
+  it('without prefix and request method does not match', () => {
+    var mockRequest = {
+      headers: { 'content-type': 'application/grpc-web-text' },
+      method: 'GET',
+      path: '/test/path'
+    }
+    expect(isApplicable(mockRequest)).toBe(false)
+  })
+
+  it('with prefix and request match', () => {
+    var mockRequest = {
+      headers: { 'content-type': 'application/grpc-web-text' },
+      method: 'POST',
+      path: '/prefix/path'
+    }
+    expect(isApplicable(mockRequest, '/prefix')).toBe(true)
+  })
+
+  it('with prefix and request does not match', () => {
+    var mockRequest = {
+      headers: { 'content-type': 'application/grpc-web-text' },
+      method: 'POST',
+      path: '/test/path'
+    }
+    expect(isApplicable(mockRequest, '/prefix')).toBe(false)
+  })
+})
